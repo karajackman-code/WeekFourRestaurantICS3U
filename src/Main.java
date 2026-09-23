1986534089ab570d96830f1cb69c39e7cbcbf926
karajackman-code <kara.jackman@stu.ocsb.ca>
2026-09-23 09:55:38 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -30,30 +30,76 @@ public class Main {
 		System.out.println("Welcome to");
 		System.out.println("Would you like a drink to start?");
 		String drinkyn = in.nextLine();
-		if (drinkyn.equalsIgnoreCase("yes")) {
-				System.out.println("Perfect! You can choose from the options below, just type in what drink you want");
+		String drink="";
+		if (drinkyn.startsWith("n")||drinkyn.startsWith("N")) {
+				System.out.println("Sounds good, what would you like to eat?");
+		} else {
+				System.out.println("Perfect! You can choose from the options below, just type in what drink you want, and how many (item in first line, quantity in next)");
 				System.out.println("Water-$1.00");
 				System.out.println("Soda-$2.50");
-				//add options for soda and milk
 				System.out.println("Milk-$1.50");
 				System.out.println("Coffee-$3.00");
 				System.out.println("Lemonade-$2.00");
 				String drinkitem = in.nextLine();
+				//milk and soda options
+				if (drinkitem.equalsIgnoreCase("Soda")) {
+				System.out.println("What type of Soda would you like? We have,");
+				System.out.println("Coca-Cola");
+				System.out.println("Fanta");
+				System.out.println("Root Beer");
+				System.out.println("Sprite");
+				System.out.println("Dr. Pepper");
+				drink = in.nextLine();
+				drink = drink.toLowerCase();
+				if (drink.startsWith("c")) {
+					drink="Coca-Cola";
+				}else if (drink.startsWith("f")) {
+					drink="Fanta";
+				}else if (drink.startsWith("r")) {
+					drink="Root Beer";
+				}else if (drink.startsWith("s")) {
+					drink="Sprite";
+				}else if (drink.startsWith("d")) {
+					drink="Dr.Pepper";
+				}else {
+					System.out.println("Sorry thats not available, no drink for you");
+				}}
+				if (drinkitem.equalsIgnoreCase("Milk")) {
+					System.out.println("What type of milk would you like?");
+					System.out.println("We have almond, chocolate, strawberry, and regular milk");
+					drink = in.nextLine();
+					drink = drink.toLowerCase();
+					if (drink.startsWith("a")) {
+						drink="Almond Milk";
+					}else if (drink.startsWith("c")) {
+						drink="Chocolate Milk";
+					}else if (drink.startsWith("s")) {
+						drink="Strawberry Milk";
+					}else if (drink.startsWith("r")) {
+						drink="Regular Milk";
+					}else {
+						System.out.println("Sorry thats not available, no drink for you");
+					}
+				}
+				//prices
 				if (drinkitem.equalsIgnoreCase("Water")) {
 					double drinkprice = (1.00);
+					drink = ("Water");
 				}
 				if (drinkitem.equalsIgnoreCase("Soda")) {
 					double drinkprice = (2.50);
 				}
 				if (drinkitem.equalsIgnoreCase("Coffee")) {
 					double drinkprice = (3.00);
+					drink = ("Coffee");
 				}
 				if (drinkitem.equalsIgnoreCase("Lemonade")) {
 					double drinkprice = (2.00);
+					drink = ("Lemonade");
 				}
-		} else {
-				System.out.println("Sounds good, what would you like to eat?");
-			}
+				if (drinkitem.equalsIgnoreCase("Milk")) {
+					double drinkprice = (1.50);
+				}
+		}
 	}
-
 }
